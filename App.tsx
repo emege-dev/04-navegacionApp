526cdee950632e55b2162bd3bf510b76037898b7
emege-dev <enrique.melida@gmail.com>
2022-04-18 23:26:03 -0400

    -

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import React from 'react'
 import { Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
-import { StackNavigator } from './navigators/StackNavigator';
+import { StackNavigator } from './src/navigators/StackNavigator';
 
 
 
